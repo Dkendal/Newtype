ab4f9c6bd218d40822c10dab7dcd4cd399157874
Dylan Kendal <dylankendal@gmail.com>
2022-05-14 17:35:39 -0400

    add basic test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,30 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import Data.Text
+import Newtype.Parser hiding (main)
+import Newtype.Syntax
+import Prettyprinter
+import Test.Tasty
+import Test.Tasty.HUnit
+import Text.Megaparsec
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = defaultMain tests
+
+assertPretty :: Text -> String -> Assertion
+assertPretty input output =
+  case parse pProgram "" input of
+    Left a ->
+      assertFailure (errorBundlePretty a)
+    Right a ->
+      show (pretty a) @?= output
+
+tests :: TestTree
+tests =
+  testGroup
+    "Parser tests"
+    [ testCase "Pretty printing" $
+        assertPretty "type A = 1" "type A = 1"
+    ]
