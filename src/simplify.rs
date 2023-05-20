8b3fda62d0343d1c3994bd071d83d53eccf5abd7
Dylan C. Kendal <dylankendal@gmail.com>
2023-05-20 14:20:55 -0400

    transform wip

--- src/simplify.rs
+++ src/simplify.rs
@@ -50,8 +50,49 @@ impl Transform for Node {
             }
             Node::Tuple(vec) => Node::Tuple(map(vec)),
             Node::Array(vec) => Node::Array(transb(vec)),
-            Node::IfExpr(_, _, _) => todo!(),
-            _ => self.clone(),
+            Node::IfExpr(cond, then, els) => {
+                Node::IfExpr(transb(cond), transb(then), els.as_ref().map(|v| transb(v)))
+            }
+            Node::BinOp { lhs, op, rhs } => Node::BinOp {
+                lhs: transb(lhs),
+                op: op.clone(),
+                rhs: transb(rhs),
+            },
+            Node::ExtendsBinOp { lhs, op, rhs } => Node::ExtendsBinOp {
+                lhs: transb(lhs),
+                op: op.clone(),
+                rhs: transb(rhs),
+            },
+            Node::ExtendsExpr(lhs, rhs, then, els) => {
+                Node::ExtendsExpr(transb(lhs), transb(rhs), transb(then), transb(els))
+            }
+            Node::ObjectLiteral(props) => Node::ObjectLiteral(
+                props
+                    .into_iter()
+                    .map(|prop| {
+                        let mut p = prop.clone();
+                        p.value = trans(&prop.value);
+                        p
+                    })
+                    .collect(),
+            ),
+            Node::Application(name, args) => Node::Application(name.clone(), map(args)),
+
+            Node::None
+            | Node::Error(_)
+            | Node::Never
+            | Node::Any
+            | Node::Unknown
+            | Node::Null
+            | Node::Undefined
+            | Node::False
+            | Node::True
+            | Node::Ident(_)
+            | Node::Number(_)
+            | Node::Primitive(_)
+            | Node::String(_)
+            | Node::TemplateString(_)
+            | Node::Infer(_) => self.clone(),
         };
 
         f(&out)
